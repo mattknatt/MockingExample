04f120d708873661b3855863b29fa0c628db9074
mattknatt <mattiashagstrommusic@gmail.com>
2026-02-05 10:00:17 +0100

    Implements test for adding items to ShoppingCart

--- src/main/java/com/example/shop/ShoppingCart.java
+++ src/main/java/com/example/shop/ShoppingCart.java
@@ -1,4 +1,17 @@
 package com.example.shop;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
+    public List<Item> items = new ArrayList<>();
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
 }
--- src/test/java/com/example/shop/ShoppingCartTest.java
+++ src/test/java/com/example/shop/ShoppingCartTest.java
@@ -1,7 +1,20 @@
 package com.example.shop;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class ShoppingCartTest {
+    
+    @Test
+    void addItem_shouldSaveItemToCart() {
+        ShoppingCart cart = new ShoppingCart();
+        Item item = new Item();
+
+        cart.addItem(item);
+
+        assertThat(cart.getItems()).contains(item);
+
+    }
 
 }
